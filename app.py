b2b304c6b63aceb4d54e39f00ab537df29e04268
CyberKestral <kbyte79@gmail.com>
2025-12-17 15:17:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template, request
+from model import train_model
+from predictor import predict_price
 
+app = Flask(__name__)
+model, scaler = train_model()
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    price = None
+    if request.method == "POST":
+        stock = request.form["stock"]
+        price = predict_price(model, scaler, stock)
+    return render_template("index.html", price=price)
+
+if __name__ == "__main__":
+    app.run(debug=True)
