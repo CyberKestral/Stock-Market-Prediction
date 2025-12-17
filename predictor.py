6656ee16542c088485108ed170b6b5578eca5948
CyberKestral <kbyte79@gmail.com>
2025-12-17 15:17:37 +0530

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,1 +1,11 @@
+import yfinance as yf
+import numpy as np
 
+def predict_price(model, scaler, stock="AAPL"):
+    data = yf.download(stock, period="3mo")['Close'].values.reshape(-1, 1)
+    data = scaler.transform(data)
+
+    last_60 = np.array([data[-60:]])
+    prediction = model.predict(last_60)
+
+    return round(float(scaler.inverse_transform(prediction)[0][0]), 2)
